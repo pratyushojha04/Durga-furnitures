90dc031ca7465c05193bd6ddaa1254fb481802cb
Pratyush ojha <82443757+pratyushojha04@users.noreply.github.com>
2025-10-19 14:26:34 +0530

    lifespan

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://durga-furnitures-2.onrender.com/","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
